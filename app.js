5425e6a3693a0b0cf7f69729de09cca1c4cb2e75
azaelsanchez <cem20619@gmail.com>
2019-11-16 00:36:34 +0100

    Busqueda de peliculas y de id realizadas

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+const mongoose = require('mongoose');
+const MovieModel = require('./models/Movie');
 
-var app = express();
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use(logger('dev'));
+ app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/movie', indexRouter);
+// app.use('/users', usersRouter);
+
+mongoose.connect("/mongodb://localhost:27017/NeflixOld",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex:true
+    })
+.then(() => console.log('conectado a mongodb'))
+.catch(error => console.log('Error al conectar a MongoDB ' + error));
+
+ 
+// Routers
+app.get('/movie',(req,res) =>{
+    MovieModel.find({})
+        .then(movieFind => res.send(movieFind))
+        .catch(error => console.log(error))
+})
+
+app.get("/movie/:id",(req,res)=>{
+    id=req.params.id;
+    console.log(id)
+    MovieModel.findById(id,(err,movie)=>{
+        if(err){
+            console.log("hay un error")
+            return res.status(500).send({mesaje: err})
+        }
+        if(!id){
+            console.log("Nada encontrado")
+            return res.status(500).send({mesaje:"Pelicula no encontrada"})
+        }
+        console.log("la pelicula es" + movie)
+        res.send(movie)
+    })
+})
+
+app.listen(3002, () =>console.log ("Server Funcionando en el puerto 3002"));
 
 module.exports = app;
---
+++ models/Movie.js
@@ -0,0 +1,33 @@
+const mongoose = require ('mongoose');
+
+const MovieSchema = mongoose.Schema({
+    popularity: Number,
+    vote_count: Number,
+    video: Boolean,
+    poster_path: {
+        type:String,
+        required: true
+        },
+    id: {
+    type:String,
+    required:true},
+    adult: Boolean,
+    backdrop_path: String,
+    original_language: String,
+    original_title: {
+        type:String,
+        required:true},
+    genre_ids: {
+        type:Array,
+        ref: 'Genero',
+        required: true},
+    title: {
+        type:String,
+        required:true},
+    vote_average: Number,
+    overview: String,
+    release_date: Number
+})
+
+const MovieModel = mongoose.model('movie',MovieSchema);
+module.exports=MovieModel;
